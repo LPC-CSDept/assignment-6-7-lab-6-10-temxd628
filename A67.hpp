a7d679ebb92589f885d52e941416bfed1e84faa5
Tem <dxpetrenko840@zonemail.clpccd.edu>
2022-10-28 20:41:25 -0700

    3 swaps

--- A67.hpp
+++ A67.hpp
@@ -7,3 +7,26 @@ using namespace std;
 // complete the Overloading function swap( )
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
+
+void swap(int &a, int &b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(int &a, int &b, int &c) {
+    if (a > b)
+        swap(a, b);
+    if (a > c)
+        swap(a, c);
+    if (b > c)
+        swap (b, c);
+
+}
+
+void swap(int &a, int &b, int &c, int &d) {
+    swap(a, b);
+    swap(b, c);
+    swap(c, d);
+}
